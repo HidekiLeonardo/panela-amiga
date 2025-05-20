10ef6f06c4b1e7e49a4b7b995f842609dc8922de
HidekiLeonardo <leonardohideki778@gmail.com>
2025-05-20 18:42:29 -0300

    feat: Update the controller with data mock

--- backend/src/main/java/com/hideki/panela_amiga/controller/ReceitaController.java
+++ backend/src/main/java/com/hideki/panela_amiga/controller/ReceitaController.java
@@ -1,4 +1,37 @@
 package com.hideki.panela_amiga.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
 public class ReceitaController {
+    // Adicionar Receita
+    @PostMapping("/adicionar")
+    public String addReceita() {
+        return "Receita adicionada";
+    }
+
+    // Mostrar Receita (ID)
+    @GetMapping("/mostrar/id")
+    public String mostrarReceita() {
+        return "Receita";
+    }
+
+    // Mostar Receitas
+    @GetMapping("/mostrar")
+    public String mostrarTodasReceitas() {
+        return "Receitas";
+    }
+    // Alterar Receita
+    @PutMapping("/mostrar")
+    public String alterarReceita() {
+        return "Receita Alterada";
+    }
+
+    // Deletar Receita
+    @DeleteMapping("/deletar/id")
+    public String deletarReceita() {
+        return "Receita Deletada";
+    }
 }

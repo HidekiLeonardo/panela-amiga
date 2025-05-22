0975e4cc4d458138593e452f5b96f87dba933b0c
HidekiLeonardo <leonardohideki778@gmail.com>
2025-05-22 19:58:50 -0300

    feat: implement ReceitaService

---
+++ backend/src/main/java/com/hideki/panela_amiga/service/ReceitaService.java
@@ -0,0 +1,27 @@
+package com.hideki.panela_amiga.service;
+
+import com.hideki.panela_amiga.model.ReceitaModel;
+import com.hideki.panela_amiga.repository.ReceitaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.awt.*;
+import java.util.List;
+
+@Service
+public class ReceitaService {
+
+    @Autowired
+    private ReceitaRepository receitaRepository;
+
+    // Adicionar Receita
+
+    // Mostrar Receita (ID)
+
+    // Mostar Receitas
+
+    // Alterar Receita
+
+    // Deletar Receita
+
+}

8aabbd6adff63c72cda83a5b82ecd76c381ef934
HidekiLeonardo <leonardohideki778@gmail.com>
2025-06-04 16:29:26 -0300

    feat(service): implement full CRUD operations for Receita entity

--- backend/src/main/java/com/hideki/panela_amiga/service/ReceitaService.java
+++ backend/src/main/java/com/hideki/panela_amiga/service/ReceitaService.java
@@ -5,8 +5,8 @@ import com.hideki.panela_amiga.repository.ReceitaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.awt.*;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ReceitaService {
@@ -15,13 +15,33 @@ public class ReceitaService {
     private ReceitaRepository receitaRepository;
 
     // Adicionar Receita
+    public ReceitaModel addReceita(ReceitaModel receita) {
+        return receitaRepository.save(receita);
+    }
 
     // Mostrar Receita (ID)
+    public ReceitaModel mostrarReceita(Long id){
+        Optional<ReceitaModel> receita = receitaRepository.findById(id);
+        return receita.orElse(null);
+    }
 
     // Mostar Receitas
+    public List<ReceitaModel> mostrarTodasReceitas(){
+        return receitaRepository.findAll();
+    }
 
     // Alterar Receita
+    public ReceitaModel alterarReceita(Long id, ReceitaModel receita) {
+        if (receitaRepository.existsById(id)) {
+            receita.setId(id);
+            receitaRepository.save(receita);
+        }
+        return null;
+    }
 
     // Deletar Receita
+    public void deletarReceita(Long id){
+        receitaRepository.deleteById(id);
+    }
 
 }

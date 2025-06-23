c0ff4b1f570f04770ad17d1f354b6dc29bc66688
HidekiLeonardo <leonardohideki778@gmail.com>
2025-06-23 18:37:23 -0300

    refactor: change controller methods to use ResponseEntity for better HTTP status control

--- backend/src/main/java/com/hideki/panela_amiga/controller/ReceitaController.java
+++ backend/src/main/java/com/hideki/panela_amiga/controller/ReceitaController.java
@@ -3,6 +3,8 @@ package com.hideki.panela_amiga.controller;
 import com.hideki.panela_amiga.dto.ReceitaDTO;
 import com.hideki.panela_amiga.service.ReceitaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,31 +18,52 @@ public class ReceitaController {
 
     // Adicionar Receita
     @PostMapping
-    public ReceitaDTO addReceita(@RequestBody ReceitaDTO receita) {
-        return receitaService.addReceita(receita);
+    public ResponseEntity<String> addReceita(@RequestBody ReceitaDTO receita) {
+        ReceitaDTO receitaDTO = receitaService.addReceita(receita);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("A receita " + receitaDTO.getNome() + " foi adicionada com sucesso!");
     }
 
     // Mostrar Receita (ID)
     @GetMapping("/{id}")
-    public ReceitaDTO mostrarReceita(@PathVariable Long id) {
-        return receitaService.mostrarReceita(id);
+    public ResponseEntity<?> mostrarReceita(@PathVariable Long id) {
+        ReceitaDTO receitaDTO = receitaService.mostrarReceita(id);
+        if (receitaDTO != null) {
+            return ResponseEntity.ok(receitaDTO);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("A receita com o id " + id + " não existe. Tente novamente...");
+        }
     }
 
     // Mostar Receitas
     @GetMapping
-    public List<ReceitaDTO> mostrarTodasReceitas() {
-        return receitaService.mostrarTodasReceitas();
+    public ResponseEntity<List<ReceitaDTO>> mostrarTodasReceitas() {
+        List<ReceitaDTO> receitaDTOS = receitaService.mostrarTodasReceitas();
+        return ResponseEntity.ok(receitaDTOS);
     }
 
     // Alterar Receita
     @PutMapping("/{id}")
-    public ReceitaDTO alterarReceita(@PathVariable Long id, @RequestBody ReceitaDTO receita) {
-        return receitaService.alterarReceita(id, receita);
+    public ResponseEntity<?> alterarReceita(@PathVariable Long id, @RequestBody ReceitaDTO receita) {
+        ReceitaDTO receitaDTO = receitaService.alterarReceita(id, receita);
+        if (receitaDTO != null) {
+            return ResponseEntity.ok(receitaDTO);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("A receita com o id " + id + " não existe. Tente novamente...");
+        }
     }
 
     // Deletar Receita
     @DeleteMapping("/{id}")
-    public void deletarReceita(@PathVariable Long id) {
-        receitaService.deletarReceita(id);
+    public ResponseEntity<String> deletarReceita(@PathVariable Long id) {
+        if (receitaService.mostrarReceita(id) != null) {
+            receitaService.deletarReceita(id);
+            return ResponseEntity.ok("A receita com o id " + id + " foi deletada com sucesso!");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("A receita com o id " + id + " não existe. Tente novamente...");
+        }
     }
 }
